bc16133466b6a1b8a4598e916d43ef348630f3bf
JoselynMMendozaM <159175891+JoselynMMendozaM@users.noreply.github.com>
2024-06-10 12:02:25 -0600

    Add files via upload

---
+++ js/ejercicio-1.js
@@ -0,0 +1,13 @@
+class triangulo{
+    constructor(base, altura){
+        this.base = base;
+        this.altura = altura;
+
+        const inputElement = document.getElementById('myInput');
+        const inputObject = new inputElement.constructor(); // Create an instance of the input element's class
+        const inputValue = inputObject.value; // Access the input value
+
+    }
+}
+
+calcularArea()
